4f8d8e7115d34c52fab33a6fddbc76c12b1de460
TaiChiPandaBoxing <809457175@qq.com>
2018-08-26 17:34:32 +0800

    init project

---
+++ src/views/login/login.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="container">
+    login container
+  </div>
+</template>
+
+<script type="text/ecmascript-6">
+export default {
+  data () {
+    return {
+    }
+  },
+  created () {
+    console.log(1111111)
+  },
+  components: {
+  }
+}
+</script>
+
+<style scoped lang="scss">
+</style>
